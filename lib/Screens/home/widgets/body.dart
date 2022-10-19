7f77dd34f60a3e168a30793e6e3893125a4f8218
Mozart Rueckert <suporte4@grupoinovasse.com.br>
2022-10-18 22:03:51 -0300

    feat: criado body da home page

---
+++ lib/Screens/home/widgets/body.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:song_flix/components/index.dart';
+
+class HomeBody extends StatefulWidget {
+  const HomeBody({Key? key}) : super(key: key);
+
+  @override
+  State<HomeBody> createState() => _HomeBodyState();
+}
+
+class _HomeBodyState extends State<HomeBody> {
+  @override
+  Widget build(BuildContext context) {
+    return Background(child: Container());
+  }
+}
