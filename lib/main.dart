8a0e47050a287b942f8b1bb208fc818f9ce4a7f5
Mozart Rueckert <suporte4@grupoinovasse.com.br>
2022-10-19 23:36:29 -0300

    feat: configurado fonte padrao para Roboto

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:song_flix/Screens/index.dart';
+import 'package:song_flix/config/index.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,9 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(fontFamily: fTextBody),
       home: const HomeScreen(),
     );
   }
