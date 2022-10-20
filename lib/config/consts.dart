742620353299c948310b3274f6e64605503f3b41
Mozart Rueckert <suporte4@grupoinovasse.com.br>
2022-10-19 22:01:28 -0300

    feat: add const font family BebasNeue

--- lib/config/consts.dart
+++ lib/config/consts.dart
@@ -3,3 +3,6 @@ import 'package:flutter/cupertino.dart';
 // Colors
 const Color kBackgroundColor = Color(0xFF1A1A1A);
 const Color kTitulo = Color(0xFF2478DF);
+
+// Font Family
+const String fTitulo = "BebasNeue";
