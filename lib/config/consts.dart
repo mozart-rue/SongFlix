aebd082b3ae775a990254d5f09365e59cbc6f088
Mozart Rueckert <suporte4@grupoinovasse.com.br>
2022-10-19 22:56:48 -0300

    feat: add color texto geral

--- lib/config/consts.dart
+++ lib/config/consts.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 // Colors
 const Color kBackgroundColor = Color(0xFF1A1A1A);
 const Color kTitulo = Color(0xFF2478DF);
+const Color kTextBody = Color(0xFFFFFFFF);
 
 // Font Family
 const String fTitulo = "BebasNeue";
