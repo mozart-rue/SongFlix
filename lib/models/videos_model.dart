b4784cf7a97c8adc05158332ef1243a13bdd7df7
Mozart Rueckert <suporte4@grupoinovasse.com.br>
2022-12-23 08:32:42 -0300

    feat: add parameter youtubeData, that come from api

--- lib/models/videos_model.dart
+++ lib/models/videos_model.dart
@@ -1,12 +1,15 @@
 
 import 'package:song_flix/models/categories_model.dart';
+import 'package:song_flix/models/youtube_api_model.dart';
 
 class VideosModel {
   final String urlVideo;
   final CategoriesModel category;
+  final YoutubeApiModel youtubeData;
 
   VideosModel({
     required this.category,
     required this.urlVideo,
+    required this.youtubeData,
   });
 }
