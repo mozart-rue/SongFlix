df71457382cec2a09c9eead639460288eb295d46
Mozart Rueckert <suporte4@grupoinovasse.com.br>
2022-12-23 08:30:46 -0300

    feat: changes database tables, added columns, create table channels

--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -3,12 +3,22 @@ import 'package:path/path.dart';
 
 Future<Database> createDatabase() async {
   final String dbPath = await getDatabasesPath();
-  final String path = join(dbPath, "devDbSongFlix.db");
+  final String path = join(dbPath, "devFlixDB.db");
 
   return openDatabase(
     path,
     onCreate: (db, version) {
-      db.execute("CREATE TABLE videos ( id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, category_id INTEGER, url TEXT) ");
+      // db.execute("CREATE TABLE videos ( id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, category_id INTEGER, url TEXT) ");
+      db.execute("CREATE TABLE videos ( "
+            "id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "
+            "category_id INTEGER, " 
+            "url TEXT, "
+            "channel_id TEXT, "
+            "title TEXT, "
+            "description TEXT, "
+            "published_at TEXT )");
+
+      db.execute("CREATE TABLE channels ( id TEXT PRIMARY KEY, name TEXT)");
 
       db.execute( 'CREATE TABLE categories ( id INTEGER, name TEXT, color INTEGER ) ');
     },
