aba9f49df506fdc7744d20332dd4baf5d313def0
Mozart Rueckert <suporte4@grupoinovasse.com.br>
2022-12-23 08:31:42 -0300

    feat: add parameter title

--- lib/components/video_item_recomended.dart
+++ lib/components/video_item_recomended.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:song_flix/config/consts.dart';
 import 'package:song_flix/models/categories_model.dart';
 import 'package:song_flix/components/index.dart';
 import 'package:song_flix/services/get_img_url.dart';
@@ -6,9 +7,13 @@ import 'package:song_flix/services/get_img_url.dart';
 class VideoItemRecomended extends StatelessWidget {
   final CategoriesModel category;
   final String videoUrl;
+  final String videoTitle;
+  final void Function()? onClick;
   const VideoItemRecomended({
     required this.category,
     required this.videoUrl,
+    required this.videoTitle,
+    this.onClick,
     Key? key}) : super(key: key);
 
   @override
@@ -28,9 +33,18 @@ class VideoItemRecomended extends StatelessWidget {
         ),
         SizedBox(height: size.height * 0.01,),
         GestureDetector(
+          onTap: onClick,
           child: Image.network(
             imgUrl,
             width: size.width * 0.9,
+          ),
+        ),
+        Text(
+          videoTitle,
+          overflow: TextOverflow.clip,
+          style: const TextStyle(
+            color: kHintText,
+            fontSize: fsText,
           ),
         ),
       ],
