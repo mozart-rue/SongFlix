a2ca4259108b022d77802acf31da8b956a313f6d
Mozart Rueckert <suporte4@grupoinovasse.com.br>
2022-12-26 10:52:47 -0300

    feat: add new consts

--- lib/config/consts.dart
+++ lib/config/consts.dart
@@ -2,10 +2,12 @@ import 'package:flutter/cupertino.dart';
 
 // Colors
 const Color kBackgroundColor = Color(0xFF1A1A1A);
+const Color kBackgroundContrast = Color(0xFF374151);
 const Color kTitulo = Color(0xFF2478DF);
 const Color kTextBody = Color(0xFFFFFFFF);
 const Color kInputField = Color(0xFF275FA3);
 const Color kHintText = Color(0xFFB0B0B0);
+const Color kGrey500 = Color(0xFF6b7280);
 
 // Font Family
 const String fTitulo = "BebasNeue";
