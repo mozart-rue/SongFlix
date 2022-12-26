44e0a9dcdf3bedb76d36287bc0263fb601027dfe
Mozart Rueckert <suporte4@grupoinovasse.com.br>
2022-12-26 10:54:32 -0300

    feat: add field of notes, and open url

--- lib/Screens/video_player/video_player_screen.dart
+++ lib/Screens/video_player/video_player_screen.dart
@@ -6,6 +6,8 @@ import 'package:song_flix/models/categories_model.dart';
 import 'package:song_flix/models/videos_model.dart';
 import 'package:song_flix/models/youtube_api_model.dart';
 import 'package:song_flix/services/get_img_url.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:status_alert/status_alert.dart';
 
 class VideoPlayerScreen extends StatefulWidget {
   final VideosModel video;
@@ -19,7 +21,24 @@ class VideoPlayerScreen extends StatefulWidget {
 
 class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
   bool tappedDescription = false;
+  TextEditingController notesController = TextEditingController();
   
+  Future<void> _launchUrl(Uri url) async {
+    if (!await launchUrl(url)){
+      StatusAlert.show(
+        context,
+        duration: const Duration(seconds: 5),
+        title: "Erro ao abrir",
+        subtitle: "Não conseguimos abrir a url",
+        configuration: const IconConfiguration(
+          icon: PhosphorIcons.circle_wavy_warning_fill,
+          size: 22,
+        ),
+        backgroundColor: Colors.grey[300],
+        dismissOnBackgroundTap: true,
+      );
+    }
+  }
   @override
   Widget build(BuildContext context) {
     CategoriesModel category = widget.video.category;
@@ -28,6 +47,7 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
 
     Size size = MediaQuery.of(context).size;
     String imgUrl = getImgUrl(urlVideo);
+    final Uri urlToOpen = Uri.parse(urlVideo);
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -124,10 +144,50 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
                     RoundedButton(
                       title: "Abrir no Youtube", 
                       backgroundColor: Colors.red,
-                      onClick: (){},
+                      onClick: (){
+                        _launchUrl(urlToOpen);
+                      },
                     ),
                   ],
                 ),
+                const SizedBox(height: 32,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    const Text(
+                      "Notes:",
+                      style: TextStyle(
+                        color: kHintText,
+                        fontSize: fsNormal,
+                      ),
+                    ),
+                    RoundedButton(
+                      title: "Salvar nota", 
+                      onClick: (){},
+                      backgroundColor: kGrey500,
+                    ),
+                  ],
+                ),
+                Container(
+                  width: size.width * 0.9,
+                  decoration: BoxDecoration(
+                    color: kBackgroundContrast,
+                    borderRadius: BorderRadius.circular(14),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                    child: TextField(
+                      maxLines: null,
+                      controller: notesController,
+                      style: const TextStyle(
+                        color: kHintText,
+                        fontSize: 18,
+                        height: 1.5,
+                      ),
+                    ),
+                  ),
+                ),
+                // addicionar videos recomendados da mesma categoria
               ],
             ),
           ),
